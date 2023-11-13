98f5afdb000a4432c67b0fc4fb5862241defab0a
Alexander Udeneev <mrprofu361@gmail.com>
2023-11-13 17:52:20 +0300

    delete graphs from class

--- linear_classification.py
+++ linear_classification.py
@@ -29,7 +29,7 @@ class LinearClassification:
 
         self.ordinary_steps = [np.zeros((features, 1))]
         self.aggressive_steps = [np.zeros((features, 1))]
-        
+
         self.cost_list = []
 
     def find_gradient(self):
@@ -132,83 +132,3 @@ class LinearClassification:
         F_score = 2 * precision * recall / (precision + recall)
 
         return precision, recall, F_score
-
-    
-
-    def show_graphs(self, momentum):
-        x = self.x
-        y = self.y
-
-        x_private = self.x_private
-        y_private = self.y_private
-        
-        theta = np.sum(np.array(self.old_thetas), axis=0) / len(self.old_thetas)
-
-        cost_list = self.cost_list
-
-        tmp_x = list(x[:, 1])
-        tmp_x_private = list(x_private[:, 1])
-
-        tmp_y = list(y[:, 0])
-        tmp_y_private = list(y_private[:, 0])
-
-        x_check = [elem for elem in tmp_x]
-        x_check_private = [elem for elem in tmp_x_private]
-
-        y_check = [elem for elem in tmp_y]
-        y_check_private = [elem for elem in tmp_y_private]
-
-        k, b, *_ = mnk(x_check, y_check)
-        k_private, b_private = mnk(x_check_private, y_check_private)
-
-        x_line = [0, max(max(x_check), max(x_check_private))]
-
-        k = momentum * k_private + (1 - momentum) * k
-        b = momentum * b_private + (1 - momentum) * b
-
-        y_line = [k * x_line[0] + b, k * x_line[1] + b]
-
-
-        print("predictable b =", theta[0], ", predictable k =", theta[1])
-        print("b =", b, ", k =", k)
-
-        plt.grid()
-
-        plt.plot(list(x[:, 1]), y, 'v', color="blue", label="data1")
-        plt.plot(list(x_private[:, 1]), y_private, '^', color="red", label="data2")
-
-        tmp_x = np.array(list(x) + list(x_private))
-
-        plt.plot(tmp_x[:, 1], np.dot(tmp_x, theta), color='red', label="Predictable line")
-        plt.xlabel("value")
-        plt.ylabel("class")
-
-        plt.legend()
-        plt.show()
-
-        rng = [x for x in range(len(cost_list))]
-        plt.plot(cost_list, rng)
-        plt.plot(0,0)
-        plt.grid()
-        plt.show()
-
-        return 
-    
-
-def mnk(xs, ys, showTable=False):
-    count = len(xs)
-    mx = sum(xs)/count
-    x2 = list(map( lambda x: x*x, xs))
-    mx2 = sum(x2)/count
-    my = sum(ys)/count
-    y2 = list(map( lambda x: x*x, ys))
-    xy = list(map(lambda x, y: x*y, xs, ys))
-    mxy = sum(xy)/count
-
-    if (mx2 - mx*mx) == 0:
-        k = 999999
-        return k, my - k*mx
-    
-    k = (mxy - mx*my)/(mx2 - mx*mx)
-    b = my - k*mx
-    return k, b
